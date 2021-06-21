2db817552ec377cd122d85164a326d572408fbb1
ramesh942 <rameshpawar1995@gmail.com>
2021-06-21 09:21:09 +0530

    breaking out homepage and directory/ menu-item components, finished styling our menu Item component.

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../Components/directory/directory.component";
+
+const Home = () => (
+  <div className="homepage">
+    <Directory/>
+  </div>
+);
+export default Home;
